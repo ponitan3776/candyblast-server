5ba3b9c0a33b2743dae61929585b7a2d6544d669
ponitan3776 <tarounakamura84@gmail.com>
2026-07-15 16:23:09 +0900

    Update helpers.js

--- backend/utils/helpers.js
+++ backend/utils/helpers.js
@@ -1,1 +1,9 @@
+function generateRecoveryCode() {
+  const parts = [];
+  for (let i = 0; i < 4; i++) {
+    parts.push(Math.random().toString(36).slice(2, 6).toUpperCase());
+  }
+  return parts.join('-');
+}
 
+module.exports = { generateRecoveryCode };
